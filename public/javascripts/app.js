ea4e288bb4f40e8ac097c42489c5bc203c9a53a9
AlejandroZhengZheng <ale0720199@gmail.com>
2022-02-04 22:41:46 -0500

    Update app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,12 @@
+/* 
+Author:Alejandro zhengzheng
+Student ID: 301083081
+Date:2022 - 02 -05
+ */
+//add a eventlistener to the button
 document.getElementById("cont-btn").addEventListener("click", email);
 
+//this function will alert the message and back to the first page
 function email() {
     alert("Thanks for your message");
     window.location = '/';
